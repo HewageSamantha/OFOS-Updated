7c6f087fe864cb45c4f27681db797937a492aabf
Samantha PC1 <Samantha PC1@samanthapc1.mshome.net>
2026-04-12 20:04:31 +0530

    Enhanced input validation in ReservationServlet to handle invalid and excessive user input, improving application stability

--- OFOS/src/main/java/bookingServlet/ReservationServlet.java
+++ OFOS/src/main/java/bookingServlet/ReservationServlet.java
@@ -33,6 +33,38 @@ public class ReservationServlet extends HttpServlet {
         String phone = request.getParameter("phone");
         String reservationDate = request.getParameter("reservation_date");
         
+        
+        String personsStr = request.getParameter("persons");
+
+     // Null + empty check
+     if(personsStr == null || personsStr.trim().isEmpty()) {
+         response.getWriter().println("Invalid input!");
+         return;
+     }
+
+     personsStr = personsStr.trim();
+
+     // Length validation
+     if(personsStr.length() > 3) {
+         response.getWriter().println("Input too long!");
+         return;
+     }
+
+     // Numeric validation
+     if(!personsStr.matches("\\d+")) {
+         response.getWriter().println("Invalid number!");
+         return;
+     }
+
+     // Convert safely
+     int persons = Integer.parseInt(personsStr);
+
+     // Range validation
+     if(persons <= 0 || persons > 20) {
+         response.getWriter().println("Invalid number of persons!");
+         return;
+     }
+        
      // Input validation
         if (name == null || phone == null || reservationDate == null ||
             name.trim().isEmpty() || phone.trim().isEmpty() || reservationDate.trim().isEmpty()) {
@@ -41,18 +73,11 @@ public class ReservationServlet extends HttpServlet {
             return;
         }
       
-        int persons = 0;
-
-        try {
-            persons = Integer.parseInt(request.getParameter("persons"));
-        } catch (NumberFormatException e) {
-            response.getWriter().println("Invalid number of persons!");
-            return;
-        }
-        
+               
         name = name.trim();
         phone = phone.trim();
         reservationDate = reservationDate.trim();
+       
         
      // Phone format validation (ONLY numbers, 10 digits)
         if (!phone.matches("[0-9]{10}")) {
@@ -77,6 +102,12 @@ public class ReservationServlet extends HttpServlet {
         PreparedStatement pstmt = null;
 
         response.setContentType("text/html");
+        
+        //CSP Header Issue fixing
+        
+        response.setHeader("Content-Security-Policy", 
+        	    "default-src 'self'; script-src 'self'; style-src 'self';");
+        
         PrintWriter out = response.getWriter();
 
         try {
